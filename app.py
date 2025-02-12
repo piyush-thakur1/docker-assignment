bd228dada1ee37be90e4d9ffb8a4993eae26b06c
piyush-thakur1 <piyush.thakur1@igtsolutions.com>
2025-02-12 22:00:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def home():
    return "Hello, Flask Server!"
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True
+    app.run(host='0.0.0.0', port=5000, debug=True)
